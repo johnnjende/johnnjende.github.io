11cb3c678318728f069a9c38abc5c9a800f0fe2a
alahealthgames <77376885+alahealthgames@users.noreply.github.com>
2021-01-13 10:11:33 +0100

    v.1.1

---
+++ Timed/js/questions.js
@@ -0,0 +1,134 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "The definition of Fitness is",
+  answer: "None of the above",
+  options: [
+    "The ability to run 20 kilometers without fatigue",
+    "To be able to lift 100 kilos maximum bench press",
+    "To meet the demands of your environment ",
+    "None of the above"
+  ]
+},
+  {
+  numb: 2,
+  question: "Health is",
+  answer: "All of the above",
+  options: [
+    "Physical well being",
+    "Mental well being",
+    "Social well being",
+    "All of the above"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which of these foods is the best source of calcium",
+  answer: "Yoghurt",
+  options: [
+    "apple",
+    "egg",
+    "banana",
+    "Yoghurt"
+  ]
+},
+  {
+  numb: 4,
+  question: "How much water is recommended for teens daily?",
+  answer: "1.5 to 3.3 litres",
+  options: [
+    "1.5 to 3.3 litres",
+    "1litres",
+    "3 litres",
+    "2.5 litres"
+  ]
+},
+  {
+  numb: 5,
+  question: "How are you doing?",
+  answer: "Great!",
+  options: [
+    "Not so great",
+    "Great!"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+{
+  numb: 6,
+  question: "Keeping accountable to one another's phsycial health is going to contribute to the spread of the virus",
+  answer: "False",
+  options: [
+    "True",
+    "False",
+  ]
+},
+{
+  numb: 7,
+  question: "Which statement below is correct?",
+  answer: "SARS-CoV-2 causes COVID-19",
+  options: [
+    "COVID-19 causes SARS-CoV-2",
+    "SARS-CoV-2 causes COVID-19",
+    "Omo, Covid is fake",
+    "No Idea"
+  ]
+},
+{
+  numb: 8,
+  question: "The South African mutation of the virus is:",
+  answer: "more contagious",
+  options: [
+    "COVID-20",
+    "more deadly",
+    "more contagious",
+    "more deadly and more contagious"
+  ]
+},
+{
+  numb: 9,
+  question: "Physical Exercise can help you grow new brain cells",
+  answer: "True",
+  options: [
+    "Lie",
+    "True",
+    "False",
+    "Maybe"
+  ]
+},
+{
+  numb: 10,
+  question: "For optimum performance, scientist recommend:",
+  answer: "6-8 hours of sleep",
+  options: [
+    "6-8 hours of sleep",
+    "9 hours or more of sleep",
+    "7 hours of sleep",
+    "Depends on you"
+  ]
+},
+{
+  numb: 11,
+  question: "Warning: Don't Try this at Home!!! What is the longest time experimentally proven, man has gone without sleep? ",
+  answer: "11 days",
+  options: [
+    "11 days",
+    "1 week",
+    "5 days",
+    "3 days"
+  ]
+},
+];
